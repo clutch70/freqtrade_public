43d1dfb16a50a860fc67ca2c321dfb57c1cbf434
clutch70 <67942178+clutch70@users.noreply.github.com>
2022-05-02 10:54:01 -0500

    Initial commit

---
+++ binance_getAllData.sh
@@ -0,0 +1,61 @@
+#getData.sh
+
+#Year of 2020
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20200101-20200401
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20200401-20200701
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20200701-20201001
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20201001-20210101
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20200101-20200401
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20200401-20200701
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20200701-20201001
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20201001-20210101
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20200101-20200401
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20200401-20200701
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20200701-20201001
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20201001-20210101
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20200101-20200401
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20200401-20200701
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20200701-20201001
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20201001-20210101
+
+
+
+
+#Year of 2021
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20210101-20210401
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20210401-20210701
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20210701-20211001
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20211001-20220101
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20210101-20210401
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20210401-20210701
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20210701-20211001
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20211001-20220101
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20210101-20210401
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20210401-20210701
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20210701-20211001
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20211001-20220101
+
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20210101-20210401
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20210401-20210701
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20210701-20211001
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20211001-20220101
+
+
+#Year of 2022
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20220101-20220301
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 5m --timerange 20220301-
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20220101-20220301
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 15m --timerange 20220301-
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20220101-20220301
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1h --timerange 20220301-
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20220101-20220301
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange binanceus -t 1d --timerange 20220301-
---
+++ getAllData.sh
@@ -0,0 +1,61 @@
+#getData.sh
+
+#Year of 2020
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20200101-20200401 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20200401-20200701 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20200701-20201001 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20201001-20210101 --dl-trades
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20200101-20200401 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20200401-20200701 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20200701-20201001 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20201001-20210101 --dl-trades
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20200101-20200401 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20200401-20200701 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20200701-20201001 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20201001-20210101 --dl-trades
+
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20200101-20200401 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20200401-20200701 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20200701-20201001 --dl-trades
+#docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20201001-20210101 --dl-trades
+
+
+
+
+#Year of 2021
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20210101-20210401 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20210401-20210701 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20210701-20211001 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20211001-20220101 --dl-trades
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20210101-20210401 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20210401-20210701 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20210701-20211001 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20211001-20220101 --dl-trades
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20210101-20210401 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20210401-20210701 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20210701-20211001 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20211001-20220101 --dl-trades
+
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20210101-20210401 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20210401-20210701 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20210701-20211001 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20211001-20220101 --dl-trades
+
+
+#Year of 2022
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20220101-20220301 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 5m --timerange 20220301- --dl-trades
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20220101-20220301 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 15m --timerange 20220301- --dl-trades
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20220101-20220301 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1h --timerange 20220301- --dl-trades
+
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20220101-20220301 --dl-trades
+docker-compose run --rm freqtrade download-data --user-data-dir user_data --exchange kraken -t 1d --timerange 20220301- --dl-trades
