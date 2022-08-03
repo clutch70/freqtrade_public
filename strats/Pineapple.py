a99f50dfcb4660db2747a9231080d06b70243e16
clutch70 <b.custard@outlook.com>
2022-08-03 08:16:29 -0700

    working

--- strats/Pineapple.py
+++ strats/Pineapple.py
@@ -12,15 +12,23 @@ from logging import FATAL
 
 logger = logging.getLogger(__name__)
 
-class Pineapple (IStrategy):
+
+class Pineapple(IStrategy):
     def version(self) -> str:
         return "v0.0.1"
 
     INTERFACE_VERSION = 3
 
-# Hyperopt params
+    # Hyperopt params
     min_roi = DecimalParameter(0.02, 0.2, decimals=2, default=0.05, space="buy")
     stoploss = DecimalParameter(-0.02, -0.2, decimals=2, default=-0.1, space="buy")
 
-    def populate_indicators(selfself, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['rsi'] = ta.RSI(dataframe)
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe, 14)
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return DataFrame
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return DataFrame
